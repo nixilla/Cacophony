598b17ff4a6e9eb5fa26f4097327562dede757c8
Janusz Slota <janusz.slota@nixilla.com>
2011-10-11 17:37:08 +0100

    Created models

---
+++ lib/filter/doctrine/sfCacophonyPlugin/TokenFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Token filter form.
+ *
+ * @package    cacophony
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrinePluginFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class TokenFormFilter extends PluginTokenFormFilter
+{
+  public function configure()
+  {
+  }
+}
