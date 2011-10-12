c4c59ba4e748cd74cedfa37567770552d88674f0
Janusz Slota <janusz.slota@nixilla.com>
2011-10-12 10:33:05 +0100

    Create proxy method for current provider

--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -2,4 +2,8 @@
 
 class myUser extends sfGuardSecurityUser
 {
+  public function getCurrentProvider()
+  {
+    return $this->getGuardUser()->getTokens()->get(0)->getProvider();
+  }
 }
--- apps/frontend/modules/default/templates/indexSuccess.php
+++ apps/frontend/modules/default/templates/indexSuccess.php
@@ -1,17 +1,18 @@
 <h1>Welcome to Cacophony App!</h1>
 
 <p>The purpose of this app is to show the possibilities of sfCacophonyPlugin.</p>
+<p>By looking into source code, you can find out how to use it and how it works.</p>
 
 <?php if($sf_user->isAuthenticated()): ?>
-<p>You are logged in as <?php echo $sf_user->getGuardUser()->getUsername(); ?> using provider: <?php echo ucfirst($sf_user->getGuardUser()->getTokens()->get(0)->getProvider()); ?></p>
+<p>You are logged in as <?php echo $sf_user->getGuardUser()->getUsername(); ?> using provider: <?php echo ucfirst($sf_user->getCurrentProvider()); ?></p>
 <?php endif; ?>
 
 <ul>
   <?php if($sf_user->isAuthenticated()): ?>
   <li><?php echo link_to('Logout','@sf_guard_signout'); ?></li>
-  <li><?php echo link_to('See what you can do with Twitter','@homepage'); ?></li>
-  <li><?php echo link_to('See what you can do with Vimeo','@homepage'); ?></li>
-  <li><?php echo link_to('See what you can do with Facebook','@homepage'); ?></li>
+  <?php if($sf_user->getCurrentProvider() == 'twitter'): ?><li><?php echo link_to('See what you can do with Twitter','@homepage'); ?></li><?php endif; ?>
+  <?php if($sf_user->getCurrentProvider() == 'vimeo'): ?><li><?php echo link_to('See what you can do with Vimeo','@homepage'); ?></li><?php endif; ?>
+  <?php if($sf_user->getCurrentProvider() == 'facebook'): ?><li><?php echo link_to('See what you can do with Facebook','@homepage'); ?></li><?php endif; ?>
   <?php else: ?>
   <li><?php echo link_to('Login with Twitter','@sf_cacophony_connect?provider=twitter'); ?></li>
   <li><?php echo link_to('Login with Vimeo','@sf_cacophony_connect?provider=vimeo'); ?></li>
